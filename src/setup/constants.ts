530e7fc4c47132610ae60b37dd7848c9874069f3
unknown <devmirzaofficial@gmail.com>
2025-06-26 01:17:44 +0500

    Initial push to new remote

---
+++ src/setup/constants.ts
@@ -0,0 +1,7 @@
+export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
+export const TELEGRAM_LINK = "https://t.me/ig_fk";
+export const GITHUB_LINK = "https://github.com/p-stream/p-strean";
+export const TWITTER_LINK = "https://twitter.com/SudoFlix";
+export const GA_ID = import.meta.env.VITE_GA_ID;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+// Do it clap??
