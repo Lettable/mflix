6cdf8776effd00d9361957cd83b8aaa2ccb27473
unknown <devmirzaofficial@gmail.com>
2025-06-26 08:27:37 +0500

    choar(svg, style)

--- src/pages/parts/home/AdsPart.tsx
+++ src/pages/parts/home/AdsPart.tsx
@@ -122,11 +122,7 @@ export function AdsPart(): JSX.Element | null {
             {adContentUrl[0] !== "null" && (
               <div>
                 <p className="text-xs text-type-dimmed text-center pt-2 mx-4">
-                  <a
-                    href="https://t.me/ig_fk"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                  <a href="https://t.me/ig_fk" target="_blank" rel="noreferrer">
                     {adContentUrl[0]}
                   </a>
                 </p>
