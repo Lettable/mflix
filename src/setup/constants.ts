70c82afea74369113307a887215c704f82ffc31a
Mirza <devmirzaofficial@gmail.com>
2025-06-29 11:56:41 +0500

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,8 +1,8 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const TELEGRAM_LINK = "https://t.me/ig_fk";
-export const DISCORD_LINK = "https://t.me/ig_fk";
-export const GITHUB_LINK = "https://github.com/p-stream/p-strean";
-export const TWITTER_LINK = "https://twitter.com/SudoFlix";
+export const TELEGRAM_LINK = "https://t.me/thesuized";
+export const DISCORD_LINK = "https://t.me/thesuized";
+export const GITHUB_LINK = "https://github.com/Lettable";
+export const TWITTER_LINK = "https://twitter.com";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // Do it clap??
